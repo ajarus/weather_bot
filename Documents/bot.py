21133cc49913d5f31d1429b01744934d22fa184c
ajarus <ajarus.throne@gmail.com>
2021-04-10 16:17:32 +0300

    tgbot v1

---
+++ Documents/bot.py
@@ -0,0 +1,22 @@
+import telebot
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
+bot = telebot.TeleBot("1627412626:AAFbE-0EosiHqIeWU8dC89ts4NKoZvSKXJU")
+owm = OWM('09cda72f689ecd429972aabe60a35596')
+mgr = owm.weather_manager()
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	observation = mgr.weather_at_place(message.text)
+	w = observation.weather
+	temp =w.temperature('celsius')['temp']
+	answer = "В городе " + message.text + " сейчас " + str(temp)
+	bot.send_message(message.chat.id, answer)
+
+bot.polling()
